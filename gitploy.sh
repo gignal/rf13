1c150ea2dea95d74e4b4d96c7154deb7e428c144
Jacob Friis Saxberg <jacob@saxberg.dk>
2013-06-20 10:30:25 +0200

    wip

---
+++ gitploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Change branches
+git checkout gh-pages
+
+# get stuff from master
+git checkout master -- index.html
+git checkout master -- lib
+git commit -m "Updates from master"
+
+# Push 
+git push origin gh-pages
+
+# Checkout master
+git checkout master
