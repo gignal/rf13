bf2258cbe0a970c5a8996a62302945932476a181
Jacob Friis Saxberg <jacob@saxberg.dk>
2013-06-25 03:38:46 +0200

    also images

--- gitploy.sh
+++ gitploy.sh
@@ -6,6 +6,7 @@ git checkout gh-pages
 # get stuff from master
 git checkout master -- index.html
 git checkout master -- lib
+git checkout master -- images
 git commit -m "Updates from master"
 
 # Push 
